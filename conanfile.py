bff1ab67670200a3ed7b02eb5ca79618df6ce86d
deathstroke44 <37647012+deathstroke44@users.noreply.github.com>
2024-03-18 22:06:24 -0400

    Create conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps
+from conan.tools.cmake import CMakeToolchain
+
+
+
+generators = ("CMakeToolchain", "CMakeDeps")
+
+class Discotec (ConanFile):
+    settings = ("os", "build_type", "arch", "compiler")
+
+    def requirements(self):
+        self.requires("boost/1.73.0")     
+        self.requires("gsl/2.7.1")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=2.8]")
+        
+    def generate(self):
+        deps = CMakeToolchain(self)
+        deps.check_components_exist = True
+        deps.generate()
+
+
+    def layout(self):
+        self.folders.generators = ""
