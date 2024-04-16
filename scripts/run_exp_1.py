8ba212286427fdc1d8860881de08b3ae57b20755
samin_yeaser <samin.yeaser@tigerit.com>
2024-04-16 04:05:51 -0400

    Updated scripts

---
+++ scripts/run_exp_1.py
@@ -0,0 +1,10 @@
+from dataset_config import *
+from method_config import LCCS
+import os
+import sys
+from run_ground_truth import run_ground_truth
+from run_time_recall import run_alg
+
+os.chdir('/data/kabir/similarity-search/models/lccs-lsh/build')
+if __name__ == '__main__':
+    run_alg([LCCS()], [MovieLens()], 'l2')
