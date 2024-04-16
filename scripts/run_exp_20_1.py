dabf626b8a88ff239ccc646d6bf8eef2bd9cd73b
samin_yeaser <samin.yeaser@tigerit.com>
2024-04-16 05:05:10 -0400

    Updated scripts

--- scripts/run_exp_20_1.py
+++ scripts/run_exp_20_1.py
@@ -7,7 +7,8 @@ from run_time_recall import run_alg
 
 os.chdir('/data/kabir/similarity-search/models/lccs-lsh-20/build')
 if __name__ == '__main__':
-    run_alg([LCCS()], [Msong()], 'l2')
     run_alg([LCCS()], [Deep()], 'l2')
     run_alg([LCCS()], [Glove()], 'l2')
     run_alg([LCCS()], [MNIST()], 'l2')
+    run_alg([LCCS()], [Msong()], 'l2')
+    run_alg([LCCS()], [NUSW()], 'l2')
